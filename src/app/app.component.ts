10bd5ba9436faafeb62925f3afe1b6b4a3a25a48
plvice <prawdziwypiotrek@gmail.com>
2017-04-04 22:36:01 +0200

    view child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { TodoListComponent } from './components/todolist/todolist.component';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,18 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-    
+    @ViewChild(TodoListComponent) TodoListComponent : TodoListComponent
+
     constructor() {
+      
+    }
+
+    reset() {
+      this.TodoListComponent.initTaskList();
+    }
+
+    ngAfterViewInit() {
+      console.log(this.TodoListComponent);
     }
 
 }
